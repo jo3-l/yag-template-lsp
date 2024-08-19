f8c56e035d35c161e91a265b7e4468e2bbd3b575
Joseph Liu <jo3.l.dev@outlook.com>
2024-08-19 14:42:45 -0700

    scripts: simplify diff code with set.update

--- scripts/gen-lytfs-diff.py
+++ scripts/gen-lytfs-diff.py
@@ -6,8 +6,7 @@ from typing import Set
 bundled_funcs: Set[str] = set()
 for filename in Path.glob(Path(__file__).parent.parent / "bundled-defs", "*.ydef"):
     with open(filename) as f:
-        for func in re.findall(r"^func\s+(\w+)", f.read(), re.MULTILINE):
-            bundled_funcs.add(func)
+        bundled_funcs.update(re.findall(r"^func\s+(\w+)", f.read(), re.MULTILINE))
 
 lytfs_result = subprocess.run(["lytfs"], stdout=subprocess.PIPE)
 latest_funcs = set(lytfs_result.stdout.decode("utf-8").splitlines())
